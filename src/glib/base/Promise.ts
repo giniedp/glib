a8e0377751459a40cd41325ef12e2fe4ab1ebfcc
Alexander Gräfenstein <mail@ginie.eu>
2015-08-11 23:30:41 +0200

    inject defer method on Promise object

--- src/glib/base/Promise.ts
+++ src/glib/base/Promise.ts
@@ -5,4 +5,25 @@ module Glib {
   }
 
   export var Promise:any = window["Promise"];
+
+  if (!Promise['defer']) {
+    Promise['defer'] = function(){
+      var resolved = false;
+      var resolveArg = undefined;
+      var rejected = false;
+      var rejectArg = undefined;
+      var executor = {
+        promise: null,
+        resolve: function(){ resolved = true; resolveArg = arguments; },
+        reject: function(){ rejected = true; rejectArg = arguments; }
+      };
+      executor.promise = new Promise(function(resolve, reject){
+        executor.resolve = resolve;
+        executor.reject = reject;
+        if (resolved) { resolve.apply(this, resolveArg); }
+        if (rejected) { reject.apply(this, rejectArg); }
+      });
+      return executor;
+    }
+  }
 }
