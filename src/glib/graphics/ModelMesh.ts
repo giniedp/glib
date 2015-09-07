021bf7e3429d19f1b0e79ccaca0eeef44199060d
Alexander Gräfenstein <mail@ginie.eu>
2015-09-07 21:54:47 +0200

    added bounding volumes to mesh

--- src/glib/graphics/ModelMesh.ts
+++ src/glib/graphics/ModelMesh.ts
@@ -11,8 +11,8 @@ module Glib.Graphics {
   export class ModelMesh {
     device:Device;
     gl:any;
-    boundingBox:number[];
-    boundingSphere:number[];
+    boundingBox:BoundingBox;
+    boundingSphere:BoundingSphere;
     materialId:number|string = 0;
     indexBuffer:Buffer;
     vertexBuffer:Buffer;
@@ -20,10 +20,27 @@ module Glib.Graphics {
     constructor(device:Device, params:ModelMeshOptions) {
       this.device = device;
       this.gl = device.context;
-      this.boundingBox = params.boundingBox || [0, 0, 0, 0, 0, 0];
-      this.boundingSphere = params.boundingSphere || [0, 0, 0, 0];
+
       this.materialId = params.materialId || 0;
 
+      var box:any = params.boundingBox;
+      if (box instanceof BoundingBox) {
+        this.boundingBox = box
+      } else if (Array.isArray(box)) {
+        this.boundingBox = new Glib.BoundingBox({ x:box[0], y:box[1], z:box[2] }, { x:box[3], y:box[4], z:box[5] });
+      } else {
+        this.boundingBox = new BoundingBox();
+      }
+      
+      var sphere:any = params.boundingSphere;
+      if (sphere instanceof BoundingSphere) {
+        this.boundingSphere = sphere;
+      } else if (Array.isArray(sphere)) {
+        this.boundingSphere = new Glib.BoundingSphere({ x:sphere[0], y:sphere[1], z:sphere[2] }, sphere[3]);
+      } else {
+        this.boundingSphere = new BoundingSphere();
+      }
+      
       var buffer:any = params.indexBuffer || {};
       if (buffer instanceof Buffer) {
         this.indexBuffer = buffer;
