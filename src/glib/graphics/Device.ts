d578eecb80cd69b24e883fe247bc5f9b8667eb4b
Alexander Gräfenstein <mail@ginie.eu>
2015-09-29 10:09:09 +0200

    comments and separated clear methods

--- src/glib/graphics/Device.ts
+++ src/glib/graphics/Device.ts
@@ -53,16 +53,22 @@ module Glib.Graphics {
     canvas:any;
     context:any;
 
+    /** The currently active index buffer */
     private _indexBuffer:Buffer;
+    /** The currently active vertex buffer */
     private _vertexBuffer:Buffer;
+    /** The currently active program */
     private _program:ShaderProgram;
+    /** The index buffer that is used to draw a full screen quad */
     private _quadIndexBuffer:Buffer;
+    /** The vertex buffer that is used to draw a full screen quad */
     private _quadVertexBuffer:Buffer;
+    /** The vertex buffer that is used to draw a full screen quad with flipped texture coordinates */
     private _quadVertexBufferFlipped: Buffer;
 
     capabilities:any;
     sampler:SamplerState[];
-    
+
     private _cullState:CullState;
     private _blendState:BlendState;
     private _depthState:DepthState;
@@ -73,11 +79,12 @@ module Glib.Graphics {
     private _vertexAttribArrayState:VertexAttribArrayState;
     private _registeredRenderTargets:Texture[] = [];
     private _registeredDepthBuffers:DepthBuffer[] = [];
-    
+    private _registeredFrameBuffers: FrameBuffer[] = [];
+
     private _frameBuffer:FrameBuffer;
     private _customFrameBuffer:FrameBuffer;
     private _customFrameBufferOptions:FrameBufferOptions = { textures:[], depthBuffer:null };
-    
+
     constructor(options:{
       canvas?: string|HTMLCanvasElement,
       context?: string|any,
@@ -110,55 +117,63 @@ module Glib.Graphics {
       }
     }
 
+    /** Gets a copy of the cull state parameters */
     get cullState() {
       return this._cullState.dump();
     }
+    /** Updates the cull state parameters and direclty commits the state to the GPU */
     set cullState(v:CullStateOptions) {
       this._cullState.commit(v);
     }
-    
+    /** Gets a copy of the blend state parameters */
     get blendState() {
       return this._blendState.dump();
     }
+    /** Updates the blend state parameters and direclty commits the state to the GPU */
     set blendState(v:BlendStateOptions) {
       this._blendState.commit(v);
     }
-    
+    /** Gets a copy of the depth state parameters */
     get depthState() {
       return this._depthState.dump();
     }
+    /** Updates the depth state parameters and direclty commits the state to the GPU */
     set depthState(v:DepthStateOptions) {
       this._depthState.commit(v);
     }
-    
+    /** Gets a copy of the offset state parameters */
     get offsetState() {
       return this._offsetState.dump();
     }
+    /** Updates the offset state parameters and direclty commits the state to the GPU */
     set offsetState(v:OffsetStateOptions) {
       this._offsetState.commit(v);
     }
-    
+    /** Gets a copy of the stencil state parameters */
     get stencilState() {
       return this._stencilState.dump();
     }
+    /** Updates the stencil state parameters and direclty commits the state to the GPU */
     set stencilState(v:StencilStateOptions) {
       this._stencilState.commit(v);
     }
-    
+    /** Gets a copy of the scissor state parameters */
     get scissorState() {
       return this._scissorState.dump();
     }
+    /** Updates the scissor state parameters and direclty commits the state to the GPU */
     set scissorState(v:ScissorStateOptions) {
       this._scissorState.commit(v);
     }
-    
+    /** Gets a copy of the viewport state parameters */
     get viewportState() {
       return this._viewportState.dump();
     }
+    /** Updates the viewport state parameters and direclty commits the state to the GPU */
     set viewportState(v:ViewportStateOptions) {
       this._viewportState.commit(v);
     }
-    
+
     /**
      * Clears the color, depth and stencil buffers
      * @param color
@@ -180,11 +195,11 @@ module Glib.Graphics {
         mask = mask | gl.COLOR_BUFFER_BIT;
         gl.clearColor(color[0], color[1], color[2], color[3]);
       }
-      if (typeof depth === 'number') {
+      if (depth != null) {
         mask = mask | gl.DEPTH_BUFFER_BIT;
         gl.clearDepth(depth);
       }
-      if (typeof stencil === 'number') {
+      if (stencil != null) {
         mask = mask | gl.STENCIL_BUFFER_BIT;
         gl.clearStencil(stencil);
       }
@@ -192,6 +207,74 @@ module Glib.Graphics {
       if (mask) {
         gl.clear(mask);
       }
+      return this;
+    }
+    /**
+     * Clears the color buffer
+     * @param color
+     * @returns {Glib.Graphics.Device}
+     */
+    clearColor(color: number|number[]|Color): Device {
+      var gl = this.context, mask = gl.COLOR_BUFFER_BIT;
+      if (color instanceof Color) {
+        gl.clearColor(color.x, color.y, color.z, color.w);
+      } else if (typeof color === 'number') {
+        gl.clearColor(Color.x(color), Color.y(color), Color.z(color), Color.w(color));
+      } else {
+        gl.clearColor(color[0], color[1], color[2], color[3]);
+      }
+      gl.clear(mask);
+      return this;
+    }
+    /**
+     * Clears the color and depth buffers
+     * @param color
+     * @returns {Glib.Graphics.Device}
+     */
+    clearColorDepth(color?: number|number[]|Color, depth: number = 1): Device {
+      var gl = this.context, mask = gl.COLOR_BUFFER_BIT;
+      if (color instanceof Color) {
+        gl.clearColor(color.x, color.y, color.z, color.w);
+      } else if (typeof color === 'number') {
+        gl.clearColor(Color.x(color), Color.y(color), Color.z(color), Color.w(color));
+      } else {
+        gl.clearColor(color[0], color[1], color[2], color[3]);
+      }
+      gl.clear(mask);
+      return this;
+    }
+    /**
+     * Clears the depth buffer
+     * @param color
+     * @returns {Glib.Graphics.Device}
+     */
+    clearDepth(depth: number = 1): Device {
+      var gl = this.context;
+      gl.clearDepth(depth);
+      gl.clear(gl.DEPTH_BUFFER_BIT);
+      return this;
+    }
+    /**
+     * Clears the stencil buffer
+     * @param color
+     * @returns {Glib.Graphics.Device}
+     */
+    clearStencil(stencil: number = 0): Device {
+      var gl = this.context;
+      gl.clearDepth(stencil);
+      gl.clear(gl.STENCIL_BUFFER_BIT);
+      return this;
+    }
+    /**
+     * Clears the depth and stencil buffer
+     * @param color
+     * @returns {Glib.Graphics.Device}
+     */
+    clearDepthStencil(depth: number = 1, stencil: number = 0): Device {
+      var gl = this.context;
+      gl.clearDepth(depth);
+      gl.clearDepth(stencil);
+      gl.clear(gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
       return this;
     }
 
@@ -208,14 +291,14 @@ module Glib.Graphics {
       var program = this._program;
       if (!iBuffer) {
         throw `drawIndexedPrimitives() requires an indexBuffer`
-      } 
+      }
       if (!vBuffer) {
         throw `drawIndexedPrimitives() requires a vertexBuffer`
-      } 
+      }
       if (!program) {
         throw `drawIndexedPrimitives() requires a program`
       }
-      
+
       var type = iBuffer.dataType;
       var Enum = PrimitiveType;
       primitiveType = Enum[primitiveType || Enum.TriangleList];
@@ -243,10 +326,10 @@ module Glib.Graphics {
       var program = this._program;
       if (!iBuffer) {
         throw `drawInstancedPrimitives() requires an indexBuffer`
-      } 
+      }
       if (!vBuffer) {
         throw `drawInstancedPrimitives() requires a vertexBuffer`
-      } 
+      }
       if (!program) {
         throw `drawInstancedPrimitives() requires a program`
       }
@@ -327,7 +410,7 @@ module Glib.Graphics {
         });
         this._quadVertexBuffer = vBuffer;
       }
-      
+
       this.indexBuffer = iBuffer;
       this.vertexBuffer = vBuffer;
       this.drawIndexedPrimitives();
@@ -350,11 +433,11 @@ module Glib.Graphics {
     }
 
     getBackBufferData() {
-      
+
     }
 
     getRenderTargets() {
-      
+
     }
 
     setRenderTarget(texture:Texture) {
@@ -362,9 +445,9 @@ module Glib.Graphics {
     }
 
     setRenderTargets(
-      rt01?: Texture, rt02?: Texture, rt03?: Texture, rt04?: Texture, 
-      rt05?: Texture, rt06?: Texture, rt07?: Texture, rt08?: Texture, 
-      rt09?: Texture, rt10?: Texture, rt11?: Texture, rt12?: Texture, 
+      rt01?: Texture, rt02?: Texture, rt03?: Texture, rt04?: Texture,
+      rt05?: Texture, rt06?: Texture, rt07?: Texture, rt08?: Texture,
+      rt09?: Texture, rt10?: Texture, rt11?: Texture, rt12?: Texture,
       rt13?: Texture, rt14?: Texture, rt15?: Texture, rt16?: Texture) {
         var opts = this._customFrameBufferOptions;
         opts.textures.length = arguments.length;
@@ -384,7 +467,7 @@ module Glib.Graphics {
           if (!this._customFrameBuffer) {
             this._customFrameBuffer = new FrameBuffer(this, opts);
           } else {
-            this._customFrameBuffer.setup(opts);  
+            this._customFrameBuffer.setup(opts);
           }
           this.frameBuffer = this._customFrameBuffer;
         } else {
@@ -392,7 +475,7 @@ module Glib.Graphics {
         }
       return this;
     }
-    
+
     get frameBuffer(): FrameBuffer {
       return this._frameBuffer;
     }
@@ -403,7 +486,7 @@ module Glib.Graphics {
         this._frameBuffer = buffer;
       }
     }
-    
+
     get vertexBuffer(): Buffer {
       return this._vertexBuffer;
     }
@@ -434,13 +517,13 @@ module Glib.Graphics {
         this._program = program;
       }
     }
-    
+
     private _bindAttribPointerAndLocation(vBuffer:Buffer, program:ShaderProgram, layout?:any, attributes?:any) {
       layout = layout || vBuffer.layout;
       attributes = attributes || program.attributes;
 
       var key, channel, attribute;
-      for (key in attributes) { // jshint ignore:line
+      for (key in attributes) {
         channel = layout[key];
         attribute = attributes[key];
         if (!channel) {
@@ -532,18 +615,18 @@ module Glib.Graphics {
         var inspects = Shader.inspectProgram(vSource, fSource);
         utils.extend(options, inspects);
       }
-      
+
       return new ShaderProgram(this, options);
     }
 
     createTexture(options:TextureOptions):Texture {
       return new Texture(this, options);
     }
-    
+
     createRenderTarget(options:TextureOptions):Texture {
       return new Texture(this, options);
     }
-    
+
     createTexture2D(options:TextureOptions = {}):Texture {
       options.type = TextureType.Texture2D;
       return new Texture(this, options);
@@ -565,17 +648,19 @@ module Glib.Graphics {
     createModel(options:ModelOptions):Model {
       return new Model(this, options);
     }
-    
+
     getSharedDepthBuffer(options:DepthBufferOptions) {
+      // no depthFormat no buffer
       if (!options.depthFormat) {
         return null;
       }
-      
-      for(var buffer of this._registeredDepthBuffers) {
-        if (buffer.width === options.width && buffer.height === options.height && buffer.depthFormat === options.depthFormat) {
-          return buffer;
+      // search by matching width, height and depthFormat
+      for(var item of this._registeredDepthBuffers) {
+        if (item.width === options.width && item.height === options.height && item.depthFormat === options.depthFormat) {
+          return item;
         }
       }
+      // create and register a new depth buffer
       var buffer = new Graphics.DepthBuffer(this, {
         width: options.width,
         height: options.height,
