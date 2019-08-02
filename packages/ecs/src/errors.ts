ce807c2cf81649a08a7663bb4c1e90d207e323bb
Alexander Gräfenstein <alex@binarymolecule.com>
2019-08-02 08:24:42 +0200

    DI with forwardRef

--- packages/ecs/src/errors.ts
+++ packages/ecs/src/errors.ts
@@ -1,3 +1,15 @@
 export function errorOnMissingService(key: any) {
   throw new Error(`Service '${key}' is missing`)
 }
+
+export function errorOnInjectUndefinedType(target: object, propertyKey?: string|symbol) {
+  throw new Error(
+    `undefined type detected evaluating @Inject(...) ${propertyKey.toString()} in class ${target}. Consider using forwardRef()`,
+  )
+}
+
+export function errorOnServiceAsUndefinedType(target: object) {
+  throw new Error(
+    `undefined type detected evaluating @Service({ as: ... }) on class ${target}. Consider using forwardRef()`,
+  )
+}
