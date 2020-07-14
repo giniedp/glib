3867f7f80be6baa2310cf11365603b793f797d88
Alexander Gräfenstein <alex@binarymolecule.com>
2020-07-14 10:26:07 +0200

    migration to eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    // project: 'packages/tsconfig.eslint.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', '@typescript-eslint/tslint'],
+  rules: {
+    "no-unused-vars": "off",
+    "no-undef": "off",
+    "no-dupe-class-members": "off"
+  },
+}
+
