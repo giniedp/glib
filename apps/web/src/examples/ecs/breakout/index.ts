a884a95d616a954f4bc12750a234c79ba3630094
Alexander Gräfenstein <github@ginie.eu>
2020-08-28 20:05:13 +0200

    breakout game example

--- apps/web/src/examples/ecs/breakout/index.ts
+++ apps/web/src/examples/ecs/breakout/index.ts
@@ -7,28 +7,29 @@ import {
   TransformComponent,
   MouseComponent,
   SpriteData,
-  SpriteMode,
 } from '@gglib/components'
 
 import { forwardRef, Inject, OnInit, OnUpdate, Component, Entity, OnAdded, OnSetup } from '@gglib/ecs'
-import { Color, Texture, Device, BlendState } from '@gglib/graphics'
-import { KeyboardKey } from '@gglib/input'
-import { Mat4 } from '@gglib/math'
+import { Color, Texture, BlendState } from '@gglib/graphics'
+import { Mat4, Rect } from '@gglib/math'
 import { CameraData, BasicRenderStep } from '@gglib/render'
 import { Events } from '@gglib/utils'
 import { ContentManager } from '@gglib/content'
 
+// This component will act as an asset bucket.
+// No other component actually loads assets by itself
+// but instead asks this component for textures etc.
 @Component()
 class AssetsComponent extends Events implements OnInit {
-
   @Inject(ContentManager, { from: 'root' })
   public content: ContentManager
 
+  // The only content this game needs is a sprite sheet.
   public sprites: Map<string, SpriteData> = new Map()
 
   public async onInit() {
     const [texture, sprites] = await Promise.all([
-      this.content.load<Texture>('/assets/textures/puzzle/sheet.png', Texture.Texture2D),
+      this.content.load('/assets/textures/puzzle/sheet.png', Texture.Texture2D),
       this.content.downloadJSON('/assets/textures/puzzle/sheet.json'),
     ])
     for (const item of sprites.content as Array<any>) {
@@ -41,40 +42,52 @@ class AssetsComponent extends Events implements OnInit {
 }
 
 @Component({
+  // before this component is initialized
+  // the following components are automatically installed
+  // on the entity
   install: [
-    AssetsComponent,
     RendererComponent,
     KeyboardComponent,
     MouseComponent,
+    // The AssetsComponent as declared above
+    AssetsComponent,
+    // The LogicComponent is declared later in code
+    // so the symbol is undefined at this point.
+    // We have to use `forwardRef` which will be resolved during install.
     forwardRef(() => LogicComponent),
-  ]
+  ],
 })
 class BreakoutGame extends Events implements OnAdded, OnInit, OnUpdate {
-
   @Inject(Entity)
   public entity: Entity
 
   @Inject(RendererComponent)
   public renderer: RendererComponent
 
-  @Inject(Device, { from: 'root' })
-  public device: Device
-
   @Inject(AssetsComponent)
   public assets: AssetsComponent
 
+  // The width and height in units.
+  // Weird numbers, but fit into 21/9 aspect ratio
+  // which is used for all examples
   public readonly width = 63
   public readonly height = 27
 
+  // This is a 2D game, so we use an orthographic projection
+  // that convers the whole game field
   public readonly camera: CameraData = {
     view: Mat4.createIdentity(),
     projection: Mat4.createOrthographicOffCenter(0, this.width, 0, this.height, 0, 100),
   }
 
-  public state: 'started' | 'reset' | 'running' | 'died' = 'started'
-  public scoreLeft = 0
-  public scoreRight = 0
+  // Our game state is very primitive here
+  public state: 'started' | 'reset' | 'running' | 'win' | 'died' = 'started'
 
+  // When this component is actually added to an entity
+  // we create all other game objects:
+  // - the game field
+  // - the paddle
+  // - and the ball
   public onAdded(e: Entity) {
     e.createChild((child) => {
       child.name = 'Field'
@@ -82,19 +95,19 @@ class BreakoutGame extends Events implements OnAdded, OnInit, OnUpdate {
     })
     e.createChild((child) => {
       child.name = 'Paddle'
-      child.install(PaddleComponent)
+      child.install(GameObjectComponent, { width: 5, height: 1, sprite: 'paddleBlue' })
     })
     e.createChild((child) => {
       child.name = 'Ball'
-      child.install(BallComponent)
+      child.install(GameObjectComponent, { width: 1, height: 1, sprite: 'ballGrey' })
     })
   }
 
   public async onInit() {
-    this.renderer.scene.camera = this.camera;
+    this.renderer.scene.camera = this.camera
     const renderStep = this.renderer.scene.steps[0] as BasicRenderStep
     renderStep.blendState = BlendState.AlphaBlend
-    renderStep.clearColor = 0xff2e2620 // Color.White.rgba
+    renderStep.clearColor = 0xff2e2620
   }
 
   public onUpdate() {
@@ -123,167 +136,163 @@ class LogicComponent implements OnUpdate {
   @Inject(BreakoutGame, { from: 'root' })
   public game: BreakoutGame
 
-  @Inject(forwardRef(() => BallComponent), { from: '/Ball' })
-  public ball: BallComponent
+  @Inject(MouseComponent, { from: 'root' })
+  public mouse: MouseComponent
 
-  @Inject(forwardRef(() => PaddleComponent), { from: '/Paddle' })
-  public paddle: PaddleComponent
+  @Inject(forwardRef(() => GameObjectComponent), { from: '/Ball' })
+  public ball: GameObjectComponent
+
+  @Inject(forwardRef(() => GameObjectComponent), { from: '/Paddle' })
+  public paddle: GameObjectComponent
 
   @Inject(forwardRef(() => FieldComponent), { from: '/Field' })
   public field: FieldComponent
 
   public onUpdate() {
+    if (this.game.state === 'reset') {
+      this.resetField()
+      this.resetBall()
+      return
+    }
     if (this.game.state !== 'running') {
       return
     }
 
-    // if (this.paddle1.intersects(this.ball.x, this.ball.y, this.ball.w, this.ball.w)) {
-    //   this.ball.x = this.paddle1.x + this.paddle1.w
-    //   this.ball.dy = -(this.paddle1.y - this.ball.y) / this.paddle1.h - 0.5
-    //   this.ball.dx = 1
-    // }
-    // if (this.paddle2.intersects(this.ball.x, this.ball.y, this.ball.w, this.ball.w)) {
-    //   this.ball.x = this.paddle2.x - this.ball.w
-    //   this.ball.dy = -(this.paddle2.y - this.ball.y) / this.paddle2.h - 0.5
-    //   this.ball.dx = -1
-    // }
-    // if (this.ball.y + this.ball.h > this.game.height) {
-    //   this.ball.y = this.game.height - this.ball.h
-    //   this.ball.dy = -Math.abs(this.ball.dy)
-    // }
-    // if (this.ball.y < 0) {
-    //   this.ball.y = 0
-    //   this.ball.dy = Math.abs(this.ball.dy)
-    // }
-    if (this.ball.y < 0) {
+    this.updatePlayerInput()
+    this.updateBallCollision()
+    this.checkWinOrLooseCondition()
+  }
+
+  private resetField() {
+    for (const entity of this.field.entity.children) {
+      entity.get(GameObjectComponent).isVisible = true
+    }
+  }
+
+  private resetBall() {
+    this.ball.dx = 0
+    this.ball.dy = 0
+  }
+
+  private updatePlayerInput() {
+    this.paddle.rect.x = this.game.width * this.mouse.xNormalized - this.paddle.rect.width / 2
+    this.paddle.rect.y = 2
+    if (!this.ball.dx && !this.ball.dy) {
+      if (this.mouse.buttonJustReleased(0)) {
+        this.ball.dx = 0
+        this.ball.dy = 1
+        this.mouse.mouse.lock()
+      } else {
+        this.ball.rect.setCenter(this.paddle.rect.getCenter())
+        this.ball.rect.y = this.paddle.rect.yEnd
+      }
+    }
+  }
+
+  private updateBallCollision() {
+    const ball = this.ball
+    const paddle = this.paddle
+
+    if (ball.rect.yEnd >= this.game.height) {
+      ball.dy = -Math.abs(ball.dy)
+    }
+    if (ball.rect.x < 0) {
+      ball.dx = Math.abs(ball.dx)
+    }
+    if (ball.rect.xEnd >= this.game.width) {
+      ball.dx = -Math.abs(ball.dx)
+    }
+    if (ball.rect.intersects(paddle.rect)) {
+      ball.dy = Math.abs(ball.dy)
+      ball.dx = -((paddle.rect.centerX - ball.rect.centerX) / paddle.rect.width) * 2
+    }
+    for (const entity of this.field.entity.children) {
+      const block = entity.get(GameObjectComponent)
+      if (!block.isVisible || !block.rect.intersects(ball.rect)) {
+        continue
+      }
+      block.isVisible = false
+      // hit from right side
+      if (ball.rect.x <= block.rect.xEnd && ball.rect.xEnd > block.rect.xEnd) {
+        ball.dx = Math.abs(ball.dx)
+      }
+      // hit from left side
+      if (ball.rect.xEnd >= block.rect.x && ball.rect.x < block.rect.x) {
+        ball.dx = -Math.abs(ball.dx)
+      }
+      // hot from below
+      if (ball.rect.yEnd >= block.rect.y && ball.rect.y < block.rect.y) {
+        ball.dy = -Math.abs(ball.dy)
+      }
+      // hot from above
+      if (ball.rect.y <= block.rect.yEnd && ball.rect.yEnd > block.rect.yEnd) {
+        ball.dy = Math.abs(ball.dy)
+      }
+      break
+    }
+  }
+
+  private checkWinOrLooseCondition() {
+    const fieldCleared = this.field.entity.children.every((e) => !e.get(GameObjectComponent).isVisible)
+    if (fieldCleared) {
+      this.game.state = 'win'
+    } else if (this.ball.rect.yEnd < 0) {
       this.game.state = 'died'
     }
   }
 }
 
+// A very basic game object that implements all we actually
+// need for all main objects.
 @Component({
-  install: [
-    SpriteComponent,
-    TransformComponent,
-    KeyboardComponent
-  ]
+  install: [SceneryLinkComponent, TransformComponent, SpriteComponent],
 })
-class PaddleComponent implements OnInit, OnUpdate {
-
+class GameObjectComponent implements OnSetup<any>, OnUpdate {
   @Inject(SpriteComponent)
-  public readonly renderable: SpriteComponent
+  private renderable: SpriteComponent
 
   @Inject(TransformComponent)
-  public readonly transform: TransformComponent
+  private transform: TransformComponent
 
-  @Inject(KeyboardComponent, { from: 'root' })
-  public readonly keyboard: KeyboardComponent
-
-  @Inject(BreakoutGame, { from: 'root' })
-  public readonly game: BreakoutGame
-
-  public x = 0
-  public y = 0
-  public w = 5
-  public h = 1
-
-  public async onInit() {
-    this.x = Math.floor(this.game.width / 2 - this.w / 2)
-    this.y = Math.floor(2 * this.h)
-    this.renderable.width = this.w
-    this.renderable.height = this.h
-    this.renderable.color = Color.White.rgba
-  }
-
-  public onUpdate() {
-    this.renderable.mode = SpriteMode.Default
-    this.renderable.sprite = this.game.assets.sprites.get('paddleBlue')
-    if (this.game.state === 'reset') {
-      this.x = Math.floor(this.game.width / 2 - this.w / 2)
-      this.y = Math.floor(2 * this.h)
-    }
-
-    if (this.game.state === 'running') {
-      this.x -= this.keyboard.isPressed(KeyboardKey.ArrowLeft) ? 1 : 0
-      this.x += this.keyboard.isPressed(KeyboardKey.ArrowRight) ? 1 : 0
-      this.x = Math.max(this.x, 0)
-      this.x = Math.min(this.x, this.game.width - this.w)
-    }
-
-    this.transform.setPosition(this.x, this.y, 0)
-  }
-
-  public intersects(x: number, y: number, w: number, h: number) {
-    if (this.y > y + h) {
-      return false
-    }
-    if (y > this.y + this.h) {
-      return false
-    }
-    if (this.x > x + w) {
-      return false
-    }
-    if (x > this.x + this.w) {
-      return false
-    }
-    return true
-  }
-}
-
-@Component({
-  install: [
-    SceneryLinkComponent,
-    TransformComponent,
-    SpriteComponent,
-  ]
-})
-class BallComponent implements OnInit, OnUpdate {
+  @Inject(AssetsComponent, { from: 'root' })
+  private assets: AssetsComponent
 
   @Inject(BreakoutGame, { from: 'root' })
-  public game: BreakoutGame
+  private game: BreakoutGame
 
-  @Inject(SpriteComponent)
-  public sprite: SpriteComponent
-
-  @Inject(TransformComponent)
-  public transform: TransformComponent
-
-  public async onInit() {
-    this.sprite.width = 1
-    this.sprite.height = 1
-    this.sprite.color = Color.White.rgba
-  }
-
-  public x = 0
-  public y = 0
-  public w = 1
-  public h = 1
+  public rect = new Rect(0, 0, 1, 1)
+  public z: number = 0
+  public sprite: string
+  public isVisible = true
   public dx = 0
   public dy = 0
-  public unitPerSec = 20
+
+  public onSetup(options: { x?: number; y?: number; width: number; height: number; sprite: string }) {
+    this.rect.x = options.x || 0
+    this.rect.y = options.y || 0
+    this.rect.width = options.width
+    this.rect.height = options.height
+    this.sprite = options.sprite
+  }
 
   public onUpdate(dt: number) {
-    if (this.game.state === 'reset') {
-      this.dx = Math.random() > 0.5 ? 1 : -1
-      this.dy = 0
-      this.x = this.game.width / 2
-      this.y = this.game.height / 2
-    }
-
     if (this.game.state === 'running') {
-      this.x += (this.dx * this.unitPerSec * dt) / 1000
-      this.y += (this.dy * this.unitPerSec * dt) / 1000
+      this.rect.x += (this.dx * 20 * dt) / 1000
+      this.rect.y += (this.dy * 20 * dt) / 1000
     }
-
-    this.transform.setPosition(this.x, this.y, 0)
+    if (this.isVisible) {
+      this.renderable.width = this.rect.width
+      this.renderable.height = this.rect.height
+      this.renderable.sprite = this.assets.sprites.get(this.sprite)
+    } else {
+      this.renderable.sprite = null
+    }
+    this.transform.setPosition(this.rect.x, this.rect.y, this.z)
   }
 }
 
 @Component({
-  install: [
-    SpriteComponent,
-    TransformComponent,
-  ]
+  install: [SpriteComponent, TransformComponent],
 })
 class FieldComponent implements OnInit {
   public name = 'Field'
@@ -291,22 +300,10 @@ class FieldComponent implements OnInit {
   @Inject(BreakoutGame, { from: 'root' })
   public game: BreakoutGame
 
-  @Inject(SpriteComponent)
-  public renderable: SpriteComponent
-
-  @Inject(TransformComponent)
-  public transform: TransformComponent
-
   @Inject(Entity)
   public entity: Entity
 
-
   public async onInit(e: Entity) {
-    this.renderable.width = this.game.width
-    this.renderable.height = this.game.height
-    this.renderable.color = Color.CornflowerBlue.rgba
-    this.transform.setPosition(0, 0, -1)
-
     const cols = 10
     const rows = 5
     const width = 4
@@ -320,7 +317,7 @@ class FieldComponent implements OnInit {
             y: 10 + y * 2,
             width: width,
             height: 1,
-            sprite: 'element_blue_rectangle'
+            sprite: 'element_blue_rectangle',
           })
         })
       }
@@ -328,52 +325,7 @@ class FieldComponent implements OnInit {
   }
 }
 
-@Component({
-  install: [
-    SceneryLinkComponent,
-    TransformComponent,
-    SpriteComponent
-  ]
-})
-class GameObjectComponent implements OnSetup<any>, OnUpdate {
-
-  @Inject(SpriteComponent)
-  private renderable: SpriteComponent
-
-  @Inject(TransformComponent)
-  private transform: TransformComponent
-
-  @Inject(AssetsComponent, { from: 'root' })
-  private assets: AssetsComponent
-
-  public x: number = 0
-  public y: number = 0
-  public z: number = 0
-  public width: number = 1
-  public height: number = 1
-  public sprite: string
-
-  public onSetup(options: { x: number, y: number, width: number, height: number, sprite: string }) {
-    this.x = options.x
-    this.y = options.y
-    this.width = options.width
-    this.height = options.height
-    this.sprite = options.sprite
-  }
-
-  public onUpdate() {
-    this.renderable.width = this.width
-    this.renderable.height = this.height
-    this.renderable.sprite = this.assets.sprites.get(this.sprite)
-
-    this.transform.setPosition(this.x, this.y, this.z)
-  }
-}
-
-createGame(
-  {
-    device: { canvas: document.getElementById('canvas') as HTMLCanvasElement },
-    autorun: true,
-  },
-  (e) => e.install(BreakoutGame),
-)
+createGame({
+  device: { canvas: document.getElementById('canvas') as HTMLCanvasElement },
+  autorun: true,
+}).install(BreakoutGame)
