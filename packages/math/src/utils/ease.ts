3f2371f55f6936a2b0628dc694d840ebe0b4dc72
Alexander Gräfenstein <github@ginie.eu>
2020-08-31 00:11:26 +0200

    added ease functions

---
+++ packages/math/src/utils/ease.ts
@@ -0,0 +1,51 @@
+export function easeLinear(t: number) {
+  return t
+}
+
+export function easeInQuad(t: number) {
+  return t * t
+}
+
+export function easeOutQuad(t: number) {
+  return t * (2 - t)
+}
+
+export function easeInOutQuad(t: number) {
+  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+}
+
+export function easeInCubic(t: number) {
+  return t * t * t
+}
+
+export function easeOutCubic(t: number) {
+  return --t * t * t + 1
+}
+
+export function easeInOutCubic(t: number) {
+  return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+}
+
+export function easeInQuart(t: number) {
+  return t * t * t * t
+}
+
+export function easeOutQuart(t: number) {
+  return 1 - --t * t * t * t
+}
+
+export function easeInOutQuart(t: number) {
+  return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t
+}
+
+export function easeInQuint(t: number) {
+  return t * t * t * t * t
+}
+
+export function easeOutQuint(t: number) {
+  return 1 + --t * t * t * t * t
+}
+
+export function easeInOutQuint(t: number) {
+  return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t
+}
--- packages/math/src/utils/index.ts
+++ packages/math/src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './clamp'
+export * from './ease'
 export * from './hermite'
 export * from './transformBox'
 export * from './transformCapsule'
