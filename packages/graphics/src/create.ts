516bb5b81c0a4dd21997612006ee56c85d5b8fe5
Alexander Gräfenstein <github@ginie.eu>
2021-09-23 18:16:22 +0200

    better typings for createDevice

--- packages/graphics/src/create.ts
+++ packages/graphics/src/create.ts
@@ -2,6 +2,9 @@ import { Device } from './Device'
 import { DeviceGPUOptions, DeviceGPU } from './webgpu'
 import { DeviceGLOptions, DeviceGL } from './webgl'
 
+export function createDevice(options: DeviceGLOptions): DeviceGL
+export function createDevice(options: DeviceGPUOptions): DeviceGPU
+export function createDevice(options: DeviceGLOptions | DeviceGPUOptions): DeviceGPU
 export function createDevice(options: DeviceGLOptions | DeviceGPUOptions): Device {
   if (options.context === 'gpu') {
     return new DeviceGPU(options)
